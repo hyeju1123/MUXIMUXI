25d3c6f94626fd99ffff2b3b7cef5c0e7c7ab9ae
happine2s <happine2s@sookmyung.ac.kr>
2023-01-30 13:47:42 +0900

    init BE

---
+++ backend/muximuxi/src/main/java/com/smw/muximuxi/MuximuxiApplication.java
@@ -0,0 +1,13 @@
+package com.smw.muximuxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuximuxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuximuxiApplication.class, args);
+    }
+
+}
