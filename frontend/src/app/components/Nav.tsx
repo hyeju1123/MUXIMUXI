b87ed2adb785bac4048a841da46434b5473745bd
ZUITOPIA <korhwngml123@naver.com>
2023-03-09 18:09:27 +0900

    Update tailwind css theme color

--- frontend/src/app/components/Nav.tsx
+++ frontend/src/app/components/Nav.tsx
@@ -1,71 +1,68 @@
 "use client";
 
 import styled from "styled-components";
-import { Text, Margin } from "../../styles/ui";
-import Image from "next/image";
-import originProfile from "/public/share/origin-profile.svg";
 
 const Style = {
-	Wrapper: styled.div`
-		width: 295px;
-		height: 918px;
-		padding: 47px 28px;
-		position: absolute;
-		top: 0;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		background-color: #dfd6de;
-		border-radius: 0px 0px 20px 20px;
-	`,
-	HoverBox: styled.div`
-		width: 228px;
-		height: 33px;
-		padding: 8px 15px;
-		:hover {
-			margin: 0px;
-			width: 228px;
-			height: 33px;
-			background-color: #fff5f5;
-			border-radius: 8px;
-			color: #674188;
-			cursor: pointer;
-		}
-	`,
+    Wrapper: styled.div`
+        width: 295px;
+        height: 918px;
+        padding: 47px 28px;
+        position: absolute;
+        top: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background-color: #dfd6de;
+        border-radius: 0px 0px 20px 20px;
+    `,
+    HoverBox: styled.div`
+        width: 228px;
+        height: 33px;
+        padding: 8px 15px;
+        :hover {
+            margin: 0px;
+            width: 228px;
+            height: 33px;
+            background-color: #fff5f5;
+            border-radius: 8px;
+            color: #674188;
+            cursor: pointer;
+        }
+    `,
 };
 
 const Profile = {
-	Wrapper: styled.div`
-		display: flex;
-		align-items: center;
-	`,
+    Wrapper: styled.div`
+        display: flex;
+        align-items: center;
+    `,
 };
 
 const MainMenu = {
-	Wrapper: styled.div`
-		width: 228px;
-		height: 182px;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-evenly;
-		border-top: 1px solid #947ea8;
-		border-bottom: 1px solid #947ea8;
-	`,
+    Wrapper: styled.div`
+        width: 228px;
+        height: 182px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        border-top: 1px solid #947ea8;
+        border-bottom: 1px solid #947ea8;
+    `,
 };
 
 const RecentBox = {
-	Wrapper: styled.div``,
-	ShowRecentItems: styled.div`
-		width: 228px;
-		height: 132px;
-		border: 1px solid red;
-	`,
+    Wrapper: styled.div``,
+    ShowRecentItems: styled.div`
+        width: 228px;
+        height: 132px;
+        border: 1px solid red;
+    `,
 };
 
 export default function Nav() {
-	return (
-		<>
-			{/* <Style.Wrapper>
+    return (
+        <>
+            {/* <Style.Wrapper>
                 <div>
                     <Profile.Wrapper>
                         <Image src={originProfile} alt="profile" width={28} height={28} />
@@ -88,28 +85,29 @@ export default function Nav() {
                     <Text.Body1>RECENT</Text.Body1>
                     <RecentBox.ShowRecentItems></RecentBox.ShowRecentItems>
                 </RecentBox.Wrapper>
+				
             </Style.Wrapper> */}
 
-			<div className="w-24 h-24 bg-gray">
-				<div>
-					<div>프로필사진</div>
-					<div>ZUITOPIA</div>
-				</div>
-				<div>
-					<div>MAIN</div>
-					<div>
-						<div>SEARCH</div>
-						<div>ITEMS</div>
-						<div>MEGAZINE</div>
-						<div>WISH LIST</div>
-					</div>
-				</div>
-				<div>ADD</div>
-				<div>
-					<div>RECENT</div>
-					<div>박스</div>
-				</div>
-			</div>
-		</>
-	);
+            <div className="flex-col w-3/12 h-5/6 border-4 border-red-500">
+                <div>
+                    <div className="border-4 border-green-500">프로필사진</div>
+                    <div className="border-4 border-green-500">ZUITOPIA</div>
+                </div>
+                <div>
+                    <div className="border-4 border-black-500">MAIN</div>
+                    <div>
+                        <div className="border-4 border-black-500">SEARCH</div>
+                        <div className="border-4 border-black-500">ITEMS</div>
+                        <div className="border-4 border-black-500">MEGAZINE</div>
+                        <div className="border-4 border-black-500">WISH LIST</div>
+                    </div>
+                </div>
+                <div className="border-4 border-purple-500">ADD</div>
+                <div>
+                    <div className="border-4 border-purple-500">RECENT</div>
+                    <div className="border-4 border-purple-500">박스</div>
+                </div>
+            </div>
+        </>
+    );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/app/**/*.{js,ts,jsx,tsx}",
-		"./src/pages/**/*.{js,ts,jsx,tsx}",
-		"./src/components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				beige: "#f7efe5",
-			},
-		},
-	},
-	plugins: [],
+    content: [
+        "./src/app/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                black: "#000000",
+                white: "#ffffff",
+                gray100: "#fcfcfc",
+                gray200: "#eeeef2",
+                gray300: "#dcdce0",
+                gray400: "#c2c2c7",
+                gray500: "#959599",
+                gray600: "#6c6c70",
+                gray700: "#49494d",
+                gray800: "#303033",
+                gray900: "#222223",
+                red100: "#ff7070",
+                red200: "#fa4242",
+                red300: "#f03030",
+                red400: "#c71b1b",
+                beige100: "#fffbf5",
+                beige200: "#f7efe5",
+                purple100: "#c3acd0",
+                purple200: "#AF9DBE",
+                purple300: "#674188",
+                purple400: "#947EA8",
+                purple500: "#dfd6de",
+                indigo100: "#595b7e",
+                indigo200: "#181b46",
+                plum100: "#9a6383",
+                plum200: "#7a2d59",
+                orange100: "#f39e77",
+                orange200: "#f26322",
+                yellow100: "#fde283",
+                yellow200: "#fec804",
+            },
+        },
+    },
+    plugins: [],
 };
